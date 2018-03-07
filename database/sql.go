8a83875c06bb518e7fa5d069e938b3fd1ef48f4d
Mitu217 <mtksk.26884um@gmail.com>
2018-03-08 08:50:52 +0900

    Import SQL from CSV

--- database/sql.go
+++ database/sql.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/Mitu217/tamate/schema"
 
@@ -21,12 +22,13 @@ type SQLDatabase struct {
 	Tables []*Table
 }
 
-func (db *SQLDatabase) dumpSQLTable(schema *schema.Schema) error {
+func (db *SQLDatabase) open() (*sql.DB, error) {
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", db.Server.User, db.Server.Password, db.Server.Host, db.Server.Port, db.Name)
-	cnn, err := sql.Open(db.Server.DriverName, dataSourceName)
-	if err != nil {
-		return err
-	}
+	return sql.Open(db.Server.DriverName, dataSourceName)
+}
+
+func (db *SQLDatabase) dumpSQLTable(schema *schema.Schema) error {
+	cnn, err := db.open()
 
 	// Get data
 	rows, err := cnn.Query("SELECT * FROM " + schema.Table.Name)
@@ -77,7 +79,49 @@ func (db *SQLDatabase) dumpSQLTable(schema *schema.Schema) error {
 	return nil
 }
 
-func (db *SQLDatabase) restoreSQLTable(schema *schema.Schema, data [][]string) error {
+func (db *SQLDatabase) resetSQLTable(schema *schema.Schema) error {
+	cnn, err := db.open()
+	if err != nil {
+		return err
+	}
+
+	// Truncate data
+	cnn.Query("TRUNCATE TABLE " + schema.Table.Name)
+
+	return nil
+}
+
+func (db *SQLDatabase) restoreSQLTable(schema *schema.Schema, data [][]interface{}) error {
+	cnn, err := db.open()
+	if err != nil {
+		return err
+	}
+
+	columns := make([]string, 0)
+	for _, column := range schema.Properties {
+		columns = append(columns, column.Name)
+	}
+	columns_text := strings.Join(columns, ",")
+
+	values := make([]string, len(data))
+	for i := range data {
+		value_text := make([]string, len(data[i]))
+		for j := range data[i] {
+			if schema.Properties[j].Type == "int" {
+				value_text[j] = data[i][j].(string)
+			}
+			value_text[j] = "'" + data[i][j].(string) + "'"
+		}
+		values[i] = "(" + strings.Join(value_text, ",") + ")"
+	}
+	values_text := strings.Join(values, ",")
+
+	// Truncate data
+	_, err = cnn.Query("INSERT INTO " + schema.Table.Name + " (" + columns_text + ") VALUES " + values_text)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -85,6 +129,10 @@ func (db *SQLDatabase) Dump(schema *schema.Schema) error {
 	return db.dumpSQLTable(schema)
 }
 
-func (db *SQLDatabase) Restore(schema *schema.Schema, data [][]string) error {
+func (db *SQLDatabase) Restore(schema *schema.Schema, data [][]interface{}) error {
+	err := db.resetSQLTable(schema)
+	if err != nil {
+		return err
+	}
 	return db.restoreSQLTable(schema, data)
 }
