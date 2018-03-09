47246fd9c6fe3caebb6bafcce71a284608a231af
Mitu217 <mtksk.26884um@gmail.com>
2018-03-10 00:00:48 +0900

    Apply use case csv datasource

--- datasource/csv.go
+++ datasource/csv.go
@@ -2,51 +2,99 @@ package datasource
 
 import (
 	"encoding/csv"
-	"io"
 	"os"
+
+	"github.com/Mitu217/tamate/schema"
 )
 
-func NewCSVDataSource(r io.Reader) (*CSVDataSource, error) {
-	recodes, err := csv.NewReader(r).ReadAll()
+// CSVConfig :
+type CSVConfig struct {
+	SoursePath string
+	OutputPath string
+}
+
+// CSVDataSource :
+type CSVDataSource struct {
+	Config  *CSVConfig
+	Columns []string
+	Values  [][]string
+}
+
+// NewCSVConfig :
+func NewCSVConfig(srcPath string, dstPath string) *CSVConfig {
+	config := &CSVConfig{
+		SoursePath: srcPath,
+		OutputPath: dstPath,
+	}
+	return config
+}
+
+// NewCSVDataSource :
+func NewCSVDataSource(sc schema.Schema, config *CSVConfig) (*CSVDataSource, error) {
+	if config.SoursePath == "" {
+		ds := &CSVDataSource{
+			Config: config,
+		}
+		return ds, nil
+	}
+
+	r, err := os.Open(config.SoursePath)
 	if err != nil {
 		return nil, err
 	}
-	values := make([][]interface{}, len(recodes))
-	for i, recode := range recodes {
-		value := make([]interface{}, len(recode))
-		for k, v := range recode {
-			value[k] = v
-		}
-		values[i] = value
+
+	records, err := csv.NewReader(r).ReadAll()
+	if err != nil {
+		return nil, err
 	}
+
+	// FIXME: columnsはSchemaを正とするようにデータを生成する
+	columns := records[0]
+	values := append(records[:0], records[1:]...)
 	ds := &CSVDataSource{
-		Values: values,
+		Config:  config,
+		Columns: columns,
+		Values:  values,
 	}
 	return ds, err
 }
 
-func NewCSVFileDataSource(path string) (*CSVDataSource, error) {
-	r, err := os.Open(path)
+// GetColumns :
+func (ds *CSVDataSource) GetColumns() []string {
+	return ds.Columns
+}
+
+// SetColumns :
+func (ds *CSVDataSource) SetColumns(columns []string) {
+	ds.Columns = columns
+}
+
+// GetValues :
+func (ds *CSVDataSource) GetValues() [][]string {
+	return ds.Values
+}
+
+// SetValues :
+func (ds *CSVDataSource) SetValues(values [][]string) {
+	ds.Values = values
+}
+
+// Output :
+func (ds *CSVDataSource) Output() error {
+	file, err := os.Create(ds.Config.OutputPath)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return NewCSVDataSource(r)
-}
+	defer file.Close()
 
-func (ds *CSVDataSource) OutputCSV(path string) error {
-	return nil
-}
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
-func contains(s []string, e string) int {
-	for i, v := range s {
-		if e == v {
-			return i
+	for _, value := range append([][]string{ds.Columns}, ds.Values...) {
+		err := writer.Write(value)
+		if err != nil {
+			return err
 		}
 	}
-	return -1
-}
-
-// CSV data source
-type CSVDataSource struct {
-	Values [][]interface{}
+	return nil
 }
