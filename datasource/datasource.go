51c8d1dac38770bbb3a5380acbf72cd3699df1e3
Mitu217 <mtksk.26884um@gmail.com>
2018-03-09 23:12:51 +0900

    Apply use case spreadsheets datasource

--- datasource/datasource.go
+++ datasource/datasource.go
@@ -1,35 +1,9 @@
 package datasource
 
-import (
-	"encoding/csv"
-	"os"
-
-	"github.com/Mitu217/tamate/schema"
-)
-
 type DataSource interface {
-	GetColumns() ([]string, error)
-	SetColumns([]string) error
-	GetValues() ([][]string, error)
-	SetValues([][]string) error
-	OutputCSV(schema.Schema, string) error
-}
-
-func Output(path string, data [][]string) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	for _, value := range data {
-		err := writer.Write(value)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	GetColumns() []string
+	SetColumns([]string)
+	GetValues() [][]string
+	SetValues([][]string)
+	Output() error
 }
