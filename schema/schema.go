cfef55e2d79deb7fd587e883abe554986e7ee09d
Mitu217 <mtksk.26884um@gmail.com>
2018-03-10 00:17:25 +0900

    Modify sql datastore conform new datastore template

--- schema/schema.go
+++ schema/schema.go
@@ -1,11 +1,22 @@
 package schema
 
+// Server :
+type Server struct {
+	DriverName string `json:"driver_name"`
+	Host       string `json:"host"`
+	Port       int    `json:"port"`
+	User       string `json:"user"`
+	Password   string `json:"password"`
+}
+
+// Table :
 type Table struct {
 	Name       string   `json:"name"`
 	PrimaryKey string   `json:"primary_key"`
 	UniqueKey  []string `json:"unique_key"`
 }
 
+// Column :
 type Column struct {
 	Name          string `json:"name"`
 	Type          string `json:"type"`
@@ -13,6 +24,7 @@ type Column struct {
 	AutoIncrement bool   `json:"auto_increment"`
 }
 
+// Schema :
 type Schema interface {
 	GetColumns() []Column
 	GetTableName() string
