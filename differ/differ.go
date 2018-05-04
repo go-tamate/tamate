c6465387b9849f4bb6c7e885394ad0915d35b846
Mitu217 <mtksk.26884um@gmail.com>
2018-05-05 06:37:26 +0900

    Refactor differ

--- differ/differ.go
+++ differ/differ.go
@@ -6,30 +6,29 @@ import (
 	"github.com/Mitu217/tamate/datasource/handler"
 )
 
+// TargetSchema is diff target schema struct
+type TargetSchema struct {
+	Datasource datasource.Datasource
+	SchemaName string
+}
+
 // Differ :
 type Differ struct {
-	SchemaName  string
-	LeftSource  datasource.Datasource
-	RightSource datasource.Datasource
+	Left  TargetSchema
+	Right TargetSchema
 }
 
 // NewSchemaDiffer :
-func NewSchemaDiffer(schemaName string, leftSrc datasource.Datasource, rightSrc datasource.Datasource) (*Differ, error) {
-	d := &Differ{
-		SchemaName:  schemaName,
-		LeftSource:  leftSrc,
-		RightSource: rightSrc,
-	}
-	return d, nil
+func NewSchemaDiffer(left TargetSchema, right TargetSchema) (*Differ, error) {
+	return nil, errors.New("not support NewSchemaDiffer()")
 }
 
 // NewRowsDiffer :
-func NewRowsDiffer(schemaName string, leftSrc datasource.Datasource, rightSrc datasource.Datasource) (*Differ, error) {
+func NewRowsDiffer(left TargetSchema, right TargetSchema) (*Differ, error) {
 	d := &Differ{
-		LeftSource:  leftSrc,
-		RightSource: rightSrc,
+		Left:  left,
+		Right: right,
 	}
-
 	diffColumns, err := d.diffColumns()
 	if err != nil {
 		return nil, err
@@ -37,77 +36,73 @@ func NewRowsDiffer(schemaName string, leftSrc datasource.Datasource, rightSrc da
 	if diffColumns.IsDiff() {
 		return nil, errors.New("schema between two data does not match")
 	}
-
-	d.SchemaName = schemaName
 	return d, err
 }
 
 // DiffColumns :
 func (d *Differ) diffColumns() (*DiffColumns, error) {
 	// Get Schemas
-	_, err := d.LeftSource.GetSchema(d.SchemaName)
+	srcSchema, err := d.Left.Datasource.GetSchema(d.Left.SchemaName)
 	if err != nil {
 		return nil, err
 	}
-	_, err = d.RightSource.GetSchema(d.SchemaName)
+	dstSchema, err := d.Right.Datasource.GetSchema(d.Right.SchemaName)
 	if err != nil {
 		return nil, err
 	}
 
 	// Get diff
 	diff := &DiffColumns{}
-	/*
-		for i := 0; i < 2; i++ {
-			for _, srcColumn := range srcSchemas.Columns {
-				found := false
-				for _, dstColumn := range dstSchemas.Columns {
-					if srcColumn.Name == dstColumn.Name {
-						found = true
+	for i := 0; i < 2; i++ {
+		for _, srcColumn := range srcSchema.Columns {
+			found := false
+			for _, dstColumn := range dstSchema.Columns {
+				if srcColumn.Name == dstColumn.Name {
+					found = true
 
-						// Modify
-						if i == 0 {
-							modifyColumn := schema.Column{
-								Name: srcColumn.Name,
-							}
-							modify := false
-							if srcColumn.Type != dstColumn.Type {
-								modify = true
-								modifyColumn.Type = dstColumn.Type
-							}
-							if srcColumn.NotNull != dstColumn.NotNull {
-								modify = true
-								modifyColumn.NotNull = dstColumn.NotNull
-							}
-							if srcColumn.AutoIncrement != dstColumn.AutoIncrement {
-								modify = true
-								modifyColumn.AutoIncrement = dstColumn.AutoIncrement
-							}
-
-							if modify {
-								diff.Modify = append(diff.Modify, modifyColumn)
-							}
+					// Modify
+					if i == 0 {
+						modifyColumn := handler.Column{
+							Name: srcColumn.Name,
+						}
+						modify := false
+						if srcColumn.Type != dstColumn.Type {
+							modify = true
+							modifyColumn.Type = dstColumn.Type
+						}
+						if srcColumn.NotNull != dstColumn.NotNull {
+							modify = true
+							modifyColumn.NotNull = dstColumn.NotNull
+						}
+						if srcColumn.AutoIncrement != dstColumn.AutoIncrement {
+							modify = true
+							modifyColumn.AutoIncrement = dstColumn.AutoIncrement
 						}
 
-						break
+						if modify {
+							diff.Modify = append(diff.Modify, modifyColumn)
+						}
 					}
-				}
-				if !found {
-					if i == 0 {
-						// Add
-						diff.Add = append(diff.Add, srcColumn)
-					} else {
-						// Delete
-						diff.Delete = append(diff.Delete, srcColumn)
-					}
+
+					break
 				}
 			}
-
-			// Swap
-			if i == 0 {
-				srcSchemas, dstSchemas = dstSchemas, srcSchemas
+			if !found {
+				if i == 0 {
+					// Add
+					diff.Add = append(diff.Add, srcColumn)
+				} else {
+					// Delete
+					diff.Delete = append(diff.Delete, srcColumn)
+				}
 			}
 		}
-	*/
+
+		// Swap
+		if i == 0 {
+			srcSchema, dstSchema = dstSchema, srcSchema
+		}
+	}
 
 	return diff, nil
 }
@@ -115,11 +110,11 @@ func (d *Differ) diffColumns() (*DiffColumns, error) {
 // DiffRows :
 func (d *Differ) DiffRows() (*DiffRows, error) {
 	// Get Rows
-	_, err := d.LeftSource.GetRows(d.SchemaName)
+	srcRows, err := d.Left.Datasource.GetRows(d.Left.SchemaName)
 	if err != nil {
 		return nil, err
 	}
-	_, err = d.RightSource.GetRows(d.SchemaName)
+	dstRows, err := d.Right.Datasource.GetRows(d.Right.SchemaName)
 	if err != nil {
 		return nil, err
 	}
@@ -130,59 +125,62 @@ func (d *Differ) DiffRows() (*DiffRows, error) {
 		Modify: &handler.Rows{},
 	}
 
-	/*
-		pki := d.Schema.ColumnIndex(d.Schema.PrimaryKey)
-		if pki == -1 {
-			return nil, errors.New("Primary key not found in `" + d.Schema.Name + "`")
-		}
+	// FIXME
+	pki := 0 //d.Schema.ColumnIndex(d.Schema.PrimaryKey)
+	if pki == -1 {
+		return nil, errors.New("Primary key not found in `" + d.Left.SchemaName + "`")
+	}
 
-		// Get diff
-		columnNames := d.Schema.ColumnNames()
-		for i := 0; i < 2; i++ {
-			for _, srcValue := range srcRows.Values {
-				srcPrimaryValue := srcValue[pki]
-				found := false
-				for _, dstValue := range dstRows.Values {
-					dstPrimaryValue := dstValue[pki]
-					if srcPrimaryValue == dstPrimaryValue {
-						found = true
+	// Get diff
+	schema, err := d.Left.Datasource.GetSchema(d.Left.SchemaName)
+	if err != nil {
+		return nil, err
+	}
+	columnNames := schema.ColumnNames()
+	for i := 0; i < 2; i++ {
+		for _, srcValue := range srcRows.Values {
+			srcPrimaryValue := srcValue[pki]
+			found := false
+			for _, dstValue := range dstRows.Values {
+				dstPrimaryValue := dstValue[pki]
+				if srcPrimaryValue == dstPrimaryValue {
+					found = true
 
-						// Modify
-						if i == 0 {
-							modifyValues := make([]string, len(columnNames))
-							modify := false
-							for ci := range columnNames {
-								if srcValue[ci] != dstValue[ci] {
-									// Modify column
-									modifyValues[ci] = dstValue[ci]
-									modify = true
-									break
-								}
-							}
-							if modify {
-								modifyValues[pki] = srcPrimaryValue
-								diff.Modify.Values = append(diff.Modify.Values, [][]string{modifyValues}...)
+					// Modify
+					if i == 0 {
+						modifyValues := make([]string, len(columnNames))
+						modify := false
+						for ci := range columnNames {
+							if srcValue[ci] != dstValue[ci] {
+								// Modify column
+								modifyValues[ci] = dstValue[ci]
+								modify = true
+								break
 							}
 						}
-						break
+						if modify {
+							modifyValues[pki] = srcPrimaryValue
+							diff.Modify.Values = append(diff.Modify.Values, [][]string{modifyValues}...)
+						}
 					}
-				}
-				if !found {
-					if i == 0 {
-						// Add
-						diff.Add.Values = append(diff.Add.Values, [][]string{srcValue}...)
-					} else {
-						// Delete
-						diff.Delete.Values = append(diff.Delete.Values, [][]string{srcValue}...)
-					}
+					break
 				}
 			}
-
-			// Swap
-			if i == 0 {
-				srcRows, dstRows = dstRows, srcRows
+			if !found {
+				if i == 0 {
+					// Add
+					diff.Add.Values = append(diff.Add.Values, [][]string{srcValue}...)
+				} else {
+					// Delete
+					diff.Delete.Values = append(diff.Delete.Values, [][]string{srcValue}...)
+				}
 			}
 		}
-	*/
+
+		// Swap
+		if i == 0 {
+			srcRows, dstRows = dstRows, srcRows
+		}
+	}
 	return diff, nil
 }
