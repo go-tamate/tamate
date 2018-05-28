498f27c62951cdd80340be8235d9266594845872
katsew <y.katsew@gmail.com>
2018-05-28 17:32:01 +0900

    Add composite pk test.

--- differ/differ_test.go
+++ differ/differ_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"context"
+
 	"github.com/Mitu217/tamate/datasource"
 
 	"github.com/araddon/dateparse"
@@ -117,6 +118,129 @@ func TestDiffer_DiffRows(t *testing.T) {
 		}
 	}
 
+}
+
+func TestDiffer_DiffRows_CompositeKey(t *testing.T) {
+	sc := &datasource.Schema{
+		Columns: []*datasource.Column{
+			{Name: "id", Type: datasource.ColumnTypeString},
+			{Name: "name", Type: datasource.ColumnTypeString},
+			{Name: "comment", Type: datasource.ColumnTypeString},
+		},
+		PrimaryKey: &datasource.Key{
+			KeyType:     datasource.KeyTypePrimary,
+			ColumnNames: []string{"id", "name"},
+		},
+	}
+
+	gbkl_1 := make(map[*datasource.Key][]*datasource.GenericColumnValue)
+	gbkl_1[sc.PrimaryKey] = []*datasource.GenericColumnValue{
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "id0",
+		},
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "name0",
+		},
+	}
+	gbkl_2 := make(map[*datasource.Key][]*datasource.GenericColumnValue)
+	gbkl_2[sc.PrimaryKey] = []*datasource.GenericColumnValue{
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "id1",
+		},
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "name1",
+		},
+	}
+	leftRows := []*datasource.Row{
+		{GroupByKey: gbkl_1, Values: newRowValuesFromString(map[string]string{"id": "id0", "name": "name0", "comment": "hello, world!"})},
+		{GroupByKey: gbkl_2, Values: newRowValuesFromString(map[string]string{"id": "id1", "name": "name1", "comment": "hello, world!!"})},
+	}
+
+	gbkr_1 := make(map[*datasource.Key][]*datasource.GenericColumnValue)
+	gbkr_1[sc.PrimaryKey] = []*datasource.GenericColumnValue{
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "id0",
+		},
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "name0",
+		},
+	}
+	gbkr_2 := make(map[*datasource.Key][]*datasource.GenericColumnValue)
+	gbkr_2[sc.PrimaryKey] = []*datasource.GenericColumnValue{
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "id1",
+		},
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "name1",
+		},
+	}
+	gbkr_3 := make(map[*datasource.Key][]*datasource.GenericColumnValue)
+	gbkr_3[sc.PrimaryKey] = []*datasource.GenericColumnValue{
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "id2",
+		},
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "name2",
+		},
+	}
+	gbkr_4 := make(map[*datasource.Key][]*datasource.GenericColumnValue)
+	gbkr_4[sc.PrimaryKey] = []*datasource.GenericColumnValue{
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "id3",
+		},
+		{
+			Column: &datasource.Column{Type: datasource.ColumnTypeString},
+			Value:  "name3",
+		},
+	}
+	rightRows := []*datasource.Row{
+		{GroupByKey: gbkr_1, Values: newRowValuesFromString(map[string]string{"id": "id0", "name": "name0", "comment": "hello, world!"})},
+		{GroupByKey: gbkr_2, Values: newRowValuesFromString(map[string]string{"id": "id1", "name": "name1", "comment": "hello, world."})},
+		{GroupByKey: gbkr_3, Values: newRowValuesFromString(map[string]string{"id": "id2", "name": "name2", "comment": "hello, world!!!"})},
+		{GroupByKey: gbkr_4, Values: newRowValuesFromString(map[string]string{"id": "id3", "name": "name3", "comment": "hello, world!!!!"})},
+	}
+
+	differ, err := NewDiffer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// the same (no diff)
+	{
+		diff, err := differ.DiffRows(sc, leftRows, leftRows)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(diff.Left) != 0 || len(diff.Right) != 0 {
+			t.Fatalf("expect: no row in diff, actual: diff.Left: %d, diff.Right: %d", len(diff.Left), len(diff.Right))
+		}
+	}
+
+	{
+		diff, err := differ.DiffRows(sc, leftRows, rightRows)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(diff.Left) != 1 {
+			t.Fatalf("expect: 1 row in diff.Left, actual: %d", len(diff.Left))
+		}
+		if len(diff.Right) != 3 {
+			t.Fatalf("expect: 3 rows in diff.Right, actual: %d", len(diff.Right))
+		}
+	}
 }
 
 func TestDiffer_DiffColumns(t *testing.T) {
