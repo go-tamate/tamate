03b5f3d488e5789a705984ddf52e8d0526e7ed5e
Mitu217 <mtksk.26884um@gmail.com>
2018-07-11 22:20:17 +0900

    Refactor csv datasource

--- datasource/csv.go
+++ datasource/csv.go
@@ -10,121 +10,116 @@ import (
 
 var reg = regexp.MustCompile("\\((.+?)\\)")
 
+// CSVDatasource is datasource config for csv file
 type CSVDatasource struct {
 	RootPath       string `json:"root_path"`
 	ColumnRowIndex int    `json:"column_row_index"`
 }
 
+// NewCSVDatasource is create CSVDatasource instance
 func NewCSVDatasource(rootPath string, columnRowIndex int) (*CSVDatasource, error) {
+	if columnRowIndex > 0 {
+		return nil, fmt.Errorf("columnRowIndex is invalid value: %d", columnRowIndex)
+	}
 	return &CSVDatasource{
 		RootPath:       rootPath,
 		ColumnRowIndex: columnRowIndex,
 	}, nil
 }
 
-func (ds *CSVDatasource) GetSchema(ctx context.Context, schemaName string) (*Schema, error) {
-	values, err := readCSVFromFile(ds.RootPath, schemaName)
+// GetSchema is getting schema from csv file
+func (ds *CSVDatasource) GetSchema(ctx context.Context, name string) (*Schema, error) {
+	csvValues, err := readFromFile(ds.RootPath, name)
 	if err != nil {
 		return nil, err
 	}
-	schema := &Schema{
-		Name: schemaName,
-		PrimaryKey: &Key{
-			KeyType: KeyTypePrimary,
-		},
+	primaryKey := &Key{
+		KeyType: KeyTypePrimary,
 	}
-	for rowIndex := range values {
+	cols := make([]*Column, 0)
+	for rowIndex := range csvValues {
 		if rowIndex != ds.ColumnRowIndex {
 			continue
 		}
-		columns := make([]*Column, len(values[rowIndex]))
-		for colIndex := range values[rowIndex] {
-			name := values[rowIndex][colIndex]
-			if ret := reg.FindStringSubmatch(name); len(ret) == 2 {
-				name = ret[1]
+		for colIndex := range csvValues[rowIndex] {
+			colName := csvValues[rowIndex][colIndex]
+			if ret := reg.FindStringSubmatch(colName); len(ret) >= 2 {
+				colName = ret[1]
+				primaryKey.ColumnNames = append(primaryKey.ColumnNames, colName)
 			}
-			column := &Column{
-				Name:            name,
+			cols = append(cols, &Column{
+				Name:            colName,
 				OrdinalPosition: colIndex,
 				Type:            ColumnTypeString,
-			}
-			// check primarykey
-			if values[rowIndex][colIndex] != name {
-				schema.PrimaryKey.ColumnNames = append(schema.PrimaryKey.ColumnNames, name)
-			}
-			columns[colIndex] = column
+			})
 		}
-		schema.Columns = columns
 		break
 	}
-	return schema, nil
+	return &Schema{
+		Name:       name,
+		PrimaryKey: primaryKey,
+		Columns:    cols,
+	}, nil
 }
 
+// GetRows is getting rows from csv file
 func (ds *CSVDatasource) GetRows(ctx context.Context, schema *Schema) ([]*Row, error) {
-	values, err := readCSVFromFile(ds.RootPath, schema.Name)
+	csvValues, err := readFromFile(ds.RootPath, schema.Name)
 	if err != nil {
 		return nil, err
 	}
-
-	index := 0
-	rows := make([]*Row, len(values)-1)
-	for rowIndex := range values {
-		if rowIndex == ds.ColumnRowIndex {
-			continue
+	if len(csvValues) > ds.ColumnRowIndex {
+		valuesWithoutColumn := make([][]string, len(csvValues)-1)
+		for rowIndex, csvValue := range csvValues {
+			if rowIndex < ds.ColumnRowIndex {
+				valuesWithoutColumn[rowIndex] = csvValue
+			} else if rowIndex > ds.ColumnRowIndex {
+				valuesWithoutColumn[rowIndex-1] = csvValue
+			}
 		}
-		rowValues := make(RowValues)
+		csvValues = valuesWithoutColumn
+	}
+	rows := make([]*Row, len(csvValues))
+	for rowIndex := range csvValues {
+		rowValues := make(RowValues, len(schema.Columns))
 		groupByKey := make(GroupByKey)
 		for colIndex, col := range schema.Columns {
-			columnValue := NewStringGenericColumnValue(col, values[rowIndex][colIndex])
-			rowValues[col.Name] = columnValue
+			colValue := NewStringGenericColumnValue(col, csvValues[rowIndex][colIndex])
+			rowValues[col.Name] = colValue
 			// grouping primarykey
 			for i := range schema.PrimaryKey.ColumnNames {
 				if schema.PrimaryKey.ColumnNames[i] == col.Name {
 					key := schema.PrimaryKey.String()
-					groupByKey[key] = append(groupByKey[key], columnValue)
+					groupByKey[key] = append(groupByKey[key], colValue)
 				}
 			}
 		}
-		rows[index] = &Row{GroupByKey: groupByKey, Values: rowValues}
-		index++
+		rows[rowIndex] = &Row{GroupByKey: groupByKey, Values: rowValues}
 	}
 	return rows, nil
 }
 
+// SetSchema is setting schema to csv file
 func (ds *CSVDatasource) SetSchema(ctx context.Context, schema *Schema) error {
-	rows, err := ds.GetRows(ctx, schema)
-	if err != nil {
-		return err
-	}
-	return ds.SetRows(ctx, schema, rows)
+	return fmt.Errorf("feature support")
 }
 
+// SetRows is setting rows to csv file
 func (ds *CSVDatasource) SetRows(ctx context.Context, schema *Schema, rows []*Row) error {
-	var csvRows [][]string
-	for i, row := range rows {
-		if i == ds.ColumnRowIndex {
-			csvRows = append(csvRows, schema.GetColumnNames())
-			continue
-		}
-		csvRow := make([]string, len(row.Values))
-		for k, cn := range schema.GetColumnNames() {
-			csvRow[k] = row.Values[cn].StringValue()
-		}
-		csvRows = append(csvRows, csvRow)
-	}
-	return writeCSV(ds.RootPath, csvRows)
+	return fmt.Errorf("feature support")
 }
 
-func readCSVFromFile(rootPath string, fileName string) ([][]string, error) {
-	r, err := os.Open(fmt.Sprintf("%s/%s.csv", rootPath, fileName))
+func readFromFile(rootPath string, fileName string) ([][]string, error) {
+	filePath := fmt.Sprintf("%s/%s.csv", rootPath, fileName)
+	r, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer r.Close()
-	return readCSV(csv.NewReader(r))
+	return read(csv.NewReader(r))
 }
 
-func readCSV(r *csv.Reader) ([][]string, error) {
+func read(r *csv.Reader) ([][]string, error) {
 	values, err := r.ReadAll()
 	if err != nil {
 		return nil, err
@@ -132,8 +127,9 @@ func readCSV(r *csv.Reader) ([][]string, error) {
 	return values, err
 }
 
-func writeCSV(uri string, values [][]string) error {
-	w, err := os.OpenFile(uri, os.O_WRONLY|os.O_CREATE, 0600)
+func writeToFile(rootPath string, fileName string, values [][]string) error {
+	filePath := fmt.Sprintf("%s/%s.csv", rootPath, fileName)
+	w, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return err
 	}
