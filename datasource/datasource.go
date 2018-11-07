a02090191cd445fedfa33ccb97aa0a9019d34d3c
Mitu217 <mtksk.26884um@gmail.com>
2018-11-08 06:30:18 +0900

    Merge value.go to datasource.go

--- datasource/datasource.go
+++ datasource/datasource.go
@@ -3,7 +3,12 @@ package datasource
 import (
 	"context"
 	"fmt"
+	"reflect"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/araddon/dateparse"
 )
 
 type Datasource interface {
@@ -11,6 +16,27 @@ type Datasource interface {
 	SetSchema(ctx context.Context, sc *Schema) error
 	GetRows(ctx context.Context, sc *Schema) ([]*Row, error)
 	SetRows(ctx context.Context, sc *Schema, rows []*Row) error
+}
+
+/*
+ * Key
+ */
+
+const (
+	KeyTypePrimary KeyType = iota
+	KeyTypeUnique
+	KeyTypeIndex
+)
+
+type KeyType int
+
+type Key struct {
+	KeyType     KeyType
+	ColumnNames []string
+}
+
+func (k *Key) String() string {
+	return fmt.Sprintf("%d:%v", k.KeyType, strings.Join(k.ColumnNames, ","))
 }
 
 /*
@@ -36,6 +62,15 @@ const (
 )
 
 type ColumnType int
+
+func (ct ColumnType) IsArray() bool {
+	switch ct {
+	case ColumnTypeBoolArray, ColumnTypeBytesArray, ColumnTypeDateArray, ColumnTypeDatetimeArray,
+		ColumnTypeFloatArray, ColumnTypeIntArray, ColumnTypeStringArray:
+		return true
+	}
+	return false
+}
 
 func (ct ColumnType) String() string {
 	switch ct {
@@ -75,33 +110,25 @@ func (ct ColumnType) String() string {
 }
 
 type Column struct {
-	Name            string     `json:"name"`
-	OrdinalPosition int        `json:"ordinal_position"`
-	Type            ColumnType `json:"type"`
-	NotNull         bool       `json:"not_null"`
-	AutoIncrement   bool       `json:"auto_increment"`
+	Name            string
+	OrdinalPosition int
+	Type            ColumnType
+	NotNull         bool
+	AutoIncrement   bool
 }
 
 func (c *Column) String() string {
 	return fmt.Sprintf("%s %s", c.Name, c.Type)
 }
 
-func (c *Column) IsArrayType() bool {
-	switch c.Type {
-	case ColumnTypeBoolArray, ColumnTypeBytesArray, ColumnTypeDateArray, ColumnTypeDatetimeArray,
-		ColumnTypeFloatArray, ColumnTypeIntArray, ColumnTypeStringArray:
-		return true
-	}
-	return false
-}
-
 /*
  * Schema
  */
+
 type Schema struct {
-	Name       string    `json:"name"`
-	PrimaryKey *Key      `json:"primary_key"`
-	Columns    []*Column `json:"columns"`
+	Name       string
+	PrimaryKey *Key
+	Columns    []*Column
 }
 
 func (sc *Schema) String() string {
@@ -130,7 +157,7 @@ type RowValues map[string]*GenericColumnValue
 type GroupByKey map[string][]*GenericColumnValue
 
 type Row struct {
-	GroupByKey GroupByKey `json:"-"`
+	GroupByKey GroupByKey
 	Values     RowValues
 }
 
@@ -143,22 +170,71 @@ func (r *Row) String() string {
 }
 
 /*
- * Key
+ * Column Value
  */
 
-const (
-	KeyTypePrimary KeyType = iota
-	KeyTypeUnique
-	KeyTypeIndex
-)
-
-type KeyType int
-
-type Key struct {
-	KeyType     KeyType  `json:"key_type"`
-	ColumnNames []string `json:"column_names"`
+type GenericColumnValue struct {
+	Column *Column
+	Value  interface{}
 }
 
-func (k *Key) String() string {
-	return fmt.Sprintf("%d:%v", k.KeyType, strings.Join(k.ColumnNames, ","))
+func NewGenericColumnValue(col *Column) *GenericColumnValue {
+	switch col.Type {
+	case ColumnTypeString:
+		return NewStringGenericColumnValue(col, "")
+	}
+	return nil
+}
+
+func NewStringGenericColumnValue(col *Column, s string) *GenericColumnValue {
+	return &GenericColumnValue{
+		Column: col,
+		Value:  s,
+	}
+}
+
+func (cv *GenericColumnValue) StringValue() string {
+	val := reflect.ValueOf(cv.Value)
+	if cv.Column.Type.IsArray() {
+		kind := val.Kind()
+		if kind == reflect.Array || kind == reflect.Slice {
+			vlen := val.Len()
+			ss := make([]string, vlen)
+			for i := 0; i < vlen; i++ {
+				ss[i] = fmt.Sprintf("%v", val.Index(i).Interface())
+			}
+			return strings.Join(ss, ",")
+		}
+	}
+	return fmt.Sprintf("%v", cv.Value)
+}
+
+func (cv *GenericColumnValue) TimeValue() (time.Time, error) {
+	switch cv.Value.(type) {
+	case time.Time:
+		return cv.Value.(time.Time), nil
+	default:
+		tv, err := dateparse.ParseAny(cv.StringValue())
+		if err != nil {
+			return time.Time{}, err
+		}
+		return tv, nil
+	}
+}
+
+func (cv *GenericColumnValue) BoolValue() bool {
+	switch cv.Value.(type) {
+	case bool:
+		return cv.Value.(bool)
+	default:
+		s := cv.StringValue()
+		if strings.ToLower(s) == "true" {
+			return true
+		}
+		num, err := strconv.Atoi(s)
+		if err == nil {
+			return num != 0
+		}
+	}
+	return false
 }
