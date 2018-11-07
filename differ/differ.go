3b2a1616f783286ccf9ea07b32522dacf0d94717
Mitu217 <mtksk.26884um@gmail.com>
2018-11-08 04:15:45 +0900

    [Modified] Merge diff.go to differ.go

--- differ/differ.go
+++ differ/differ.go
@@ -1,6 +1,7 @@
 package differ
 
 import (
+	"encoding/json"
 	"fmt"
 	"sort"
 	"strings"
@@ -24,6 +25,24 @@ type DiffRows struct {
 
 func (dr *DiffRows) HasDiff() bool {
 	return len(dr.Left) > 0 || len(dr.Right) > 0
+}
+
+type Diff struct {
+	Schema      *datasource.Schema `json:"schema"`
+	DiffColumns *DiffColumns       `json:"diff_columns"`
+	DiffRows    *DiffRows          `json:"diff_rows"`
+}
+
+func (d *Diff) HasDiff() bool {
+	return d.DiffColumns.HasDiff() || d.DiffRows.HasDiff()
+}
+
+func (d *Diff) ExportJSON() ([]byte, error) {
+	return json.Marshal(d)
+}
+
+func (d *Diff) ExportPrettyJSON() ([]byte, error) {
+	return json.MarshalIndent(d, "", "  ")
 }
 
 type Differ struct {
